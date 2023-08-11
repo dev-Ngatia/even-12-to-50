8ad6e0297b88c7a7f35e29fb0ab29462a8d0b21a
dev-Ngatia <64679798+dev-Ngatia@users.noreply.github.com>
2023-08-11 11:45:02 +0300

    Create 12-50-even.java

---
+++ 12-50-even.java
@@ -0,0 +1,10 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int number = 12;
+
+        do {
+            System.out.print(number + " ");
+            number += 2;
+        } while (number <= 50);
+    }
+}
